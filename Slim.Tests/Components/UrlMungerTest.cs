0812716c1ebe2ae70b5d08543b259350e49b3efb
Tyler Rooney <tyler@tylerrooney.ca>
2014-04-04 14:57:02 -0300

    Fixing tests

---
+++ Slim.Tests/Components/UrlMungerTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+using Slim.Components;
+
+namespace Slim.Tests
+{
+	[TestFixture ()]
+	public class UrlMungerTest
+	{
+		[Test ()]
+		public void MungeId ()
+		{
+			int expected = 1000;
+			int result = UrlMunger.Expand(UrlMunger.Shrink(expected));
+			Assert.AreEqual(expected, result);
+		}
+	}
+}
